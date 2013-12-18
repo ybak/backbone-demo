d88d3bab495ff59b1c5c00cdf85057900c6d3544
ybak.yf <ybakswu@gmail.com>
2013-12-18 18:29:45 +0800

    fix error

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ if ('development' == app.get('env')) {
 	app.use(express.errorHandler());
 }
 
-app.get('/', wines.index);
+app.get('/', wines.list);
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
